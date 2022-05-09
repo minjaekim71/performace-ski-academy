c0f65cb38a8097e64c48dfa24ae622a1510b6280
CubeDr <jbhyunikim@gmail.com>
2022-05-10 00:54:04 +0900

    이미지 슬라이더 가로 길이가 화면 전체를 채우도록 수정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import './App.css';
 import Header from './components/header/Header';
 import banner from './banner.jpeg';
 import SimpleImageSlider from 'react-simple-image-slider';
+import {useEffect, useState} from 'react';
 
 const bannerImages = [banner, banner, banner];
 
 function App() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        function onResize() {
+            setWidth(window.innerWidth);
+        }
+
+        window.addEventListener('resize', onResize);
+        return () => window.removeEventListener('resize', onResize);
+    });
+
     return (
         <div className="App">
             <Header/>
             <SimpleImageSlider
-                width={1024}
+                width={width}
                 height={512}
                 images={bannerImages}
                 showNavs={false}
