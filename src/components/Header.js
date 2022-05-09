d596032d1855be8e4f9194806e3257a6a574e73b
CubeDr <jbhyunikim@gmail.com>
2022-05-10 00:21:49 +0900

    제목 클릭 시 홈으로 이동

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,14 @@ import Menu from "./Menu";
 function Header() {
     return (
         <header>
-            <img className='logo' src={logo} alt={''}/>
-            <div className='text'>
-                <h1 className='title'>퍼포먼스 스키 아카데미</h1>
-                <h2 className='subtitle'>In Bears Town</h2>
-            </div>
-            <Menu />
+            <a href='/'>
+                <img className='logo' src={logo} alt={''}/>
+                <div className='text'>
+                    <h1 className='title'>퍼포먼스 스키 아카데미</h1>
+                    <h2 className='subtitle'>In Bears Town</h2>
+                </div>
+            </a>
+            <Menu/>
         </header>
     );
 }
