8bc06a1ecb3a3a222a0ffd95425bd8aa560e4000
CubeDr <jbhyunikim@gmail.com>
2022-05-10 00:41:08 +0900

    React Simple Image Slider 모듈을 이용해 이미지 슬라이더 추가
    https://www.npmjs.com/package/react-simple-image-slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 import Header from './components/header/Header';
+import banner from './banner.jpeg';
+import SimpleImageSlider from 'react-simple-image-slider';
+
+const bannerImages = [banner, banner, banner];
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <SimpleImageSlider
+                width={1024}
+                height={512}
+                images={bannerImages}
+                showNavs={false}
+                showBullets={true}
+                autoPlay={true}
+                autoPlayDelay={5.0}
+            />
         </div>
     );
 }
