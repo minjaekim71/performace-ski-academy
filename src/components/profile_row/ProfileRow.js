8343418697af4ffa6cb8a38de6e22de0ac3c9beb
CubeDr <jbhyunikim@gmail.com>
2022-05-10 15:24:48 +0900

    프로필 스타일과 SBAK, KSIA 설명 추가

--- src/components/profile_row/ProfileRow.js
+++ src/components/profile_row/ProfileRow.js
@@ -1,9 +1,17 @@
+import './ProfileRow.css';
 import Profile from '../profile/Profile';
 
 function ProfileRow({profiles}) {
     return (
-        <div>
-            {profiles.map(profile => <Profile {...profile} key={profile.name} />)}
+        <div className="profile-row">
+            {profiles.map(profile => <Profile {...profile} key={profile.name}/>)}
+            <div className="description">
+                <span className="sbak">SBAK</span>
+                <span className="sbak-kr">(한국 스키장 경영 협회 티칭 자격)</span>과
+                <span className="ksia">KSIA</span>
+                <span className="ksia-kr">(대한 스키 지도자 연맹 레벨)</span>은
+                <span className="authorized">검증받은 스키 지도자</span>들에게 부여되는 등급입니다.
+            </div>
         </div>
     );
 }
