cbcaea5c68f5bb3bf2ed9d5a50e697d6994a25a1
민재 김 <minjaekim7311@gmail.com>
2022-06-08 17:06:07 +0900

    promotion image 수정

--- src/components/Promotion/Promotion.js
+++ src/components/Promotion/Promotion.js
@@ -4,7 +4,7 @@ import link2 from './link2.jpg';
 
 function Promotion() {
     return (
-        <div className='promotion'>
+        <div className="promotion">
             <img className="image" src={link1} alt="" />
             <img className="image" src={link2} alt="" />
             
