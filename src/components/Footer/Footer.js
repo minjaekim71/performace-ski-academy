88a49fbce7ce1c67adaa31f4ff9ae67641b74250
민재 김 <minjaekim7311@gmail.com>
2022-06-08 19:20:56 +0900

    re-footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,24 +5,15 @@ function Footer() {
         <div className="footer">
             <table className="table">
                 <tbody>
-
                     <tr>
                         <th colspan="3">Performance Ski Academy</th>
                     </tr>
-
-
                     <th className="header" rowspan="2">Phone Number</th>
-
-
                     <tr>
                         <tr>010-6750-1351</tr>
                         <tr>010-7468-1355</tr>
                     </tr>
-
-
                     <th className="header" rowsapn="4">Information</th>
-
-
                     <td>
                         <tr><td className="subheader">Name </td></tr>
                         <tr><td className="subheader">President </td></tr>
@@ -35,7 +26,6 @@ function Footer() {
                         <tr>436-37-00734</tr>
                         <tr>performance_ski@naver.com</tr>
                     </td>
-
                 </tbody>
             </table>
         </div>
